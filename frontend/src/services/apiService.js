cd133aa169b8e6f552cfd0a96d595d28a7e95ead
FilipJaworski <filip.jaworski31@gmail.com>
2025-09-01 20:33:34 +0200

    feat: Change API URL for Cloud Run

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // UWAGA: Zmień ten adres URL na adres Twojej wdrożonej usługi Cloud Run, gdy będzie gotowa.
 // Na razie, testuje i użyje adresu lokalnego.
-const API_URL = 'http://localhost:8080/api'; // Przykładowy lokalny adres URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_URL,
